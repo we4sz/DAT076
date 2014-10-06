82917f6b2141d0ad3039ab94a32a34fb72fc8c35
we4sz <johanssonjohn91@gmail.com>
2014-10-06 16:38:11 +0200

    fixed login with json instead

---
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/model/UserLoginModel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chalmers.dat076.moviefinder.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author John
+ */
+public class UserLoginModel implements Serializable{
+    
+    private String username;
+    private String password;
+
+    public UserLoginModel() {
+    }
+
+    public UserLoginModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+}
