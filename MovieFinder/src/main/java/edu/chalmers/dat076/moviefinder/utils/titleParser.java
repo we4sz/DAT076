1906ef65a54aa0eba17d1f3d87e688946bf63385
Carl Jansson <carjan91@gmail.com>
2014-10-06 15:46:41 +0200

    Added titleParser class and autogenerated test file.

---
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/utils/titleParser.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.chalmers.dat076.moviefinder.utils;
+
+/**
+ * A class for parsing movie and series file names and derive a title.
+ *
+ * @author Carl Jansson
+ */
+public class titleParser {
+    
+    
+    public boolean returnTrue(){
+        return true;
+    }
+    
+}
---
+++ MovieFinder/src/test/java/edu/chalmers/dat076/moviefinder/utils/titleParserTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.chalmers.dat076.moviefinder.utils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Carl Jansson
+ */
+public class titleParserTest {
+    
+    public titleParserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of returnTrue method, of class titleParser.
+     */
+    @Test
+    public void testReturnTrue() {
+        System.out.println("returnTrue");
+        titleParser instance = new titleParser();
+        boolean expResult = true;
+        boolean result = instance.returnTrue();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
