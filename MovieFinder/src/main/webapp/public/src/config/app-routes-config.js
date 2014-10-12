2f4e20cecdd626790cc2f2276cb591c98ee0ccb8
Peter Eliasson <warackta@gmail.com>
2014-10-12 20:11:18 +0200

    Cleaned up routes and added a /login route.

--- MovieFinder/src/main/webapp/public/src/config/app-routes-config.js
+++ MovieFinder/src/main/webapp/public/src/config/app-routes-config.js
@@ -7,34 +7,36 @@
     'use strict';
 
     angular.module('movieFinder')
-            .config(function ($routeProvider, USER_ROLES) {
+            .config(function ($routeProvider, USER_ROLES, AUTH_LOGIN_PATH) {
                 $routeProvider
                         .when('/', {
                             templateUrl: 'partials/home.html'
                         })
+                        .when(AUTH_LOGIN_PATH, {
+                            templateUrl: 'partials/login.html',
+                            controller: 'LoginViewCtrl as loginViewCtrl'
+                        })
                         .when('/admin', {
                             template: '<h1>admin</h1>',
                             resolve: {
-                                auth: function(authHelper) {
-                                    return authHelper.restrictRoute([
-                                        USER_ROLES.ADMIN
-                                    ]);
-                                }
+                                auth: restrictRoute([USER_ROLES.ADMIN])
                             }
                         })
                         .when('/viewer', {
                             template: '<h1>viewer</h1>',
                             resolve: {
-                                auth: function(authHelper) {
-                                    return authHelper.restrictRoute([
-                                        USER_ROLES.VIEWER,
-                                        USER_ROLES.ADMIN
-                                    ]);
-                                }
+                                auth: restrictRoute([USER_ROLES.ADMIN, USER_ROLES.VIEWER])
                             }
                         })
                         .otherwise({
                             redirectTo: '/'
                         });
             });
+
+    // Helper function to keep the route definitions a bit tidier
+    function restrictRoute(roles) {
+        return function(authHelper) {
+            return authHelper.restrictRoute(roles);
+        };
+    }
 })();
