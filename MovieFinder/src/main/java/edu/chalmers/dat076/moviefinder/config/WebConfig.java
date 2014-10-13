82fd921a6fe47534edad3ec7f6e2f36173d7729a
Peter Eliasson <warackta@gmail.com>
2014-10-13 16:10:39 +0200

    Added java based config.
    initalizers (these are automatically found by spring):
    * ProjectWebApplicationInitializer, initializes the web servlet. (~web.xml)
    * SecurityWebApplicationInitializer, initializes the spring security. This functionallity is new.
    
    config:
    * RepositoryConfig - sets up the database. This file was previously ApplicationConfig.java.
    * SecurityConfig - sets options for spring security. Currently using anti-csrf and all available security header. Also sets custom CSP headers, as this functionallity is not yet in spring-security.
    * WebConfig - Sets up the view resolver (mapping to .jsp files) and resource routes. (~servlet-context.xml)

---
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/config/WebConfig.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chalmers.dat076.moviefinder.config;
+
+import edu.chalmers.dat076.moviefinder.service.FileThreadService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("edu.chalmers.dat076.moviefinder.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    InternalResourceViewResolver setViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // GET requests for /build/** are served static files from /build/
+        registry.addResourceHandler("/build/**")
+                .addResourceLocations("/build/")
+                .setCachePeriod(31556926);
+                
+        // TODO: The following two routes are only needed for index-dev
+        // Used for direct references instead of having to build whenever
+        // a file changes.
+        registry.addResourceHandler("/public/**")
+                .addResourceLocations("/public/");
+        registry.addResourceHandler("/partials/**")
+                .addResourceLocations("/public/partials/");
+
+        super.addResourceHandlers(registry);
+    }
+
+    @Bean(name = "FileThreadService", initMethod = "init", destroyMethod = "destory")
+    public FileThreadService fileThreadService() {
+        return new FileThreadService();
+    }
+
+}
