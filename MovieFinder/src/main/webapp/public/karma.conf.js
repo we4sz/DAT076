a11b131b849d79da4772c93c647104967ed11936
Peter Eliasson <warackta@gmail.com>
2014-10-17 10:09:35 +0200

    Reduced test verbosity.
    * Added missing config files for log4j.
    * Made Karma print less information.

--- MovieFinder/src/main/webapp/public/karma.conf.js
+++ MovieFinder/src/main/webapp/public/karma.conf.js
@@ -19,21 +19,23 @@ module.exports = function (config) {
             'test/**/*-spec.js'
         ],
         // list of files to exclude
-        exclude: [],
+        exclude: [
+            'src/util/dev-version-reminder.js'
+        ],
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {},
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['dots'],
         // web server port
         port: 9876,
         // enable / disable colors in the output (reporters and logs)
         colors: true,
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_WARN,
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
         // start these browsers
