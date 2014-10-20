1b91a2a0edd23da683a2d279c0f832556c337571
Peter Eliasson <warackta@gmail.com>
2014-10-20 15:12:05 +0200

    Changed FileSystemListener to pass both path and name of files.

--- MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/listener/FileSystemListener.java
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/listener/FileSystemListener.java
@@ -14,10 +14,10 @@ import java.util.List;
  */
 public interface FileSystemListener {
     
-    public void initFile(String paths);
+    public void initFile(String path, String name);
     
-    public void newFile(String path);
+    public void newFile(String path, String name);
     
-    public void oldPath(String path);
+    public void oldPath(String path, String name);
     
 }
