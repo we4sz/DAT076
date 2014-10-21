3c2a3c82061a7980b0280332d0d219a5581e5083
we4sz <johanssonjohn91@gmail.com>
2014-10-21 19:02:45 +0200

    fixed seriedata

--- MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/service/MovieFileDatabaseHelper.java
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/service/MovieFileDatabaseHelper.java
@@ -1,9 +1,13 @@
 package edu.chalmers.dat076.moviefinder.service;
 
 import edu.chalmers.dat076.moviefinder.model.OmdbMediaResponse;
+import edu.chalmers.dat076.moviefinder.model.TVDBData;
+import edu.chalmers.dat076.moviefinder.model.TVDBEpisode;
+import edu.chalmers.dat076.moviefinder.model.TVDBSerie;
 import edu.chalmers.dat076.moviefinder.model.TemporaryMedia;
 import edu.chalmers.dat076.moviefinder.persistence.Movie;
 import edu.chalmers.dat076.moviefinder.persistence.MovieRepository;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -20,24 +24,40 @@ public class MovieFileDatabaseHelper {
 
     @Autowired
     private OmdbHandler omdbHandler;
+    
+    @Autowired
+    private TVDBHandler tvdbHandler;
 
     @Autowired
     private MovieRepository movieRepository;
 
     public boolean saveFile(String path, String name) {
         TemporaryMedia temporaryMedia = titleParser.parseMedia(name);
-        OmdbMediaResponse omdbData = omdbHandler.getOMDB(temporaryMedia.getName());
-
-        if (omdbData == null) {
-            return false;
+        if (temporaryMedia.IsMovie()) {
+            OmdbMediaResponse omdbData = omdbHandler.getOMDB(temporaryMedia.getName(), temporaryMedia.getYear());
+            if (omdbData == null) {
+                return false;
+            } else {
+                Movie movie = new Movie(path, omdbData);
+                try {
+                    movieRepository.save(movie);
+                } catch (DataIntegrityViolationException e) {
+                    return false;
+                }
+            }
         } else {
-            Movie movie = new Movie(path, omdbData);
-            try {
-                movieRepository.save(movie);
-            } catch (DataIntegrityViolationException e) {
+            try{
+                Movie movie = new Movie(path, tvdbHandler.getEpisodeAndSerie(temporaryMedia));
+                try {
+                    movieRepository.save(movie);
+                } catch (DataIntegrityViolationException e) {
+                    return false;
+                }
+            }catch(IOException | NullPointerException e){
                 return false;
             }
         }
+
         return true;
     }
 
