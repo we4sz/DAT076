7c47407dda02c2755087e8fc95e5b7829c316887
Peter Eliasson <warackta@gmail.com>
2014-10-21 15:38:30 +0200

    Added an api/files/:id route.

--- MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/controller/FileController.java
+++ MovieFinder/src/main/java/edu/chalmers/dat076/moviefinder/controller/FileController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,5 +29,11 @@ public class FileController {
     public @ResponseBody
     Page<Movie> listMovies() {
         return movieRepository.findAll(new PageRequest(0, 25));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Movie getMovieById(@PathVariable long id) {
+        return movieRepository.findOne(id);
     }
 }
