67711809b9cb4e6cfb7785d764f0afce6932305b
we4sz <johanssonjohn91@gmail.com>
2014-10-22 15:16:11 +0200

    fixed basic functionallity for videotag

--- MovieFinder/src/main/webapp/public/src/controllers/movie-details-controller.js
+++ MovieFinder/src/main/webapp/public/src/controllers/movie-details-controller.js
@@ -5,7 +5,12 @@
 (function () {
     'use strict';
     angular.module('movieFinder.controllers')
-            .controller('MovieDetailsCtrl', function (movieDetailsCtrlData) {
+            .controller('MovieDetailsCtrl', function (movieDetailsCtrlData, $sce) {
                 this.movie = movieDetailsCtrlData;
+
+                this.video = function () {
+                    return $sce.trustAsResourceUrl("http://localhost:8084/MovieFinder/api/files/stream/" + movieDetailsCtrlData.id);
+                };
+                
             });
 })();
