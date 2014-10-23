d3dc2d06615f7e597579f5b983043c577cf9e40b
we4sz <johanssonjohn91@gmail.com>
2014-10-23 20:48:05 +0200

    added logic to add path as admin

---
+++ MovieFinder/src/main/webapp/public/src/controllers/admin-controller.js
@@ -0,0 +1,29 @@
+//
+// view-movies-controller.js
+// Holds the BrowseCtrl, controller for the browsing screen.
+//
+(function () {
+    'use strict';
+    angular.module('movieFinder.controllers')
+            .controller('AdminCtrl', function (admin) {
+                var _this = this;
+                
+                this.path = "";
+                this.error = undefined;
+                this.added = undefined;
+                this.addPath = function(){
+                    
+                    admin.addPath(_this.path).success(function(){
+                        _this.error = undefined;
+                        _this.added = "Successfully added the path";
+                    }).error(function(){
+                        _this.added = undefined;
+                        _this.error = "Not a valid path or already exists";
+                    });
+                };
+                
+                
+            });
+})();
+
+
