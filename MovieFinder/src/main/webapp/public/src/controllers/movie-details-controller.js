bd1cafebe2b4e402201b90b6238f4c9c54421b14
Hallin <antonhallin3@gmail.com>
2014-10-24 19:15:38 +0200

    Changed the old controllers
    Also added filtering to movies.

--- MovieFinder/src/main/webapp/public/src/controllers/movie-details-controller.js
+++ MovieFinder/src/main/webapp/public/src/controllers/movie-details-controller.js
@@ -1,16 +1,23 @@
 //
-// view-movies-controller.js
+// movie-details-controller.js
 // Holds the MovieDetailsCtrl, controller for the details screen.
 //
 (function () {
     'use strict';
     angular.module('movieFinder.controllers')
-            .controller('MovieDetailsCtrl', function (movieDetailsCtrlData, $sce) {
-                this.movie = movieDetailsCtrlData;
+            .controller('MovieDetailsCtrl', function (movieDetailsCtrlResolve, $sce) {
+                this.movie = movieDetailsCtrlResolve;
 
                 this.video = function () {
-                    return $sce.trustAsResourceUrl('api/files/stream/' + movieDetailsCtrlData.id);
+                    return $sce.trustAsResourceUrl('api/files/stream/' + movieDetailsCtrlResolve.id);
                 };
 
+            })
+            
+            // Resolve before MovieDetailsCtrl
+            .factory('movieDetailsCtrlResolve', function($route, movieService) {
+                return function() {
+                    return movieService.getById($route.current.params.id);
+                };
             });
 })();
--- MovieFinder/src/main/webapp/public/src/controllers/movies-controller.js
+++ MovieFinder/src/main/webapp/public/src/controllers/movies-controller.js
@@ -1,23 +1,36 @@
 //
-// view-movies-controller.js
+// movies-controller.js
 // Holds the BrowseCtrl, controller for the browsing screen.
 //
 (function () {
     'use strict';
     angular.module('movieFinder.controllers')
-            .controller('MoviesCtrl', function (movie, moviesCtrlData, $location) {
+            .controller('MoviesCtrl', function (movieService, moviesCtrlResolve, $location) {
                 var _this = this;
-                this.movies = moviesCtrlData.movies;
+                this.movies = moviesCtrlResolve.movies;
 
-                this.filterMovies = function (rating) {
-                    movie.getMoviesByFilter({imdbRating: rating}).then(function (data) {
+                this.filterMovies = function (rating, runtime) {
+                    movieService.getListByFilter({
+                        imdbRating: rating,
+                        runtime: runtime
+                    }).then(function (data) {
                         _this.movies = data;
                     });
-
                 };
 
-                this.goTo = function (path, movie) {
-                    $location.path(path+movie.id);
+                this.goTo = function (path) {
+                    $location.path(path);
+                };
+            })
+            
+            // Resolve before MovieCtrl
+            .factory('moviesCtrlResolve', function(movieService) {
+                return function() {
+                    return movieService.getList().then(function(data) {
+                        return {
+                            movies: data
+                        };
+                    });
                 };
             });
 })();
